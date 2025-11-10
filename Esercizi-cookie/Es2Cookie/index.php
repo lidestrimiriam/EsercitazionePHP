1e21add92c4f9f5d364a84bd5b2214f8425db0ff
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-11-10 13:36:49 +0100

    h

---
+++ Esercizi-cookie/Es2Cookie/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio2 Cookie</title>
+
+</head>
+<body>
+
+    <form action = "credenziali.php" method = "POST">
+    <label for = "nome">inserisci il nome</label>
+    <input type = "text" name = "nome">
+    <label for = "cognome">inserisci il cognome</label>
+    <input type = "text" name = "cognome">
+    <button type = "submit">Invia</button>
+
+    <?php
+
+
+
+    ?>
+</body>
+</html>
