1b823f37bc311597e403302289c4e0aa0a41ad98
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-11-24 12:41:18 +0100

    n

--- GestioneCarrello/index.php
+++ GestioneCarrello/index.php
@@ -1,8 +1,31 @@
-<?php
-    session_start()
+  <?php
+    session_start();
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+
+    $utenti = json_decode(file_get_contents('utenti.json'), true);
+
+    $successo = false;
+    $dati = [];
+
+    foreach ($utenti as $utente) {
+        if ($utente["login"] == $login && $utente["password"] == $password) {
+        $successo = true;
+        $dati = $utente;
+        break;
+        }
+    }
+
+    if ($successo) {
+        $_SESSION['utente'] = $dati;
+        echo "Login riuscito";
+    } else {
+        echo "Login fallito";
+    }
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +34,17 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>LOGIN</h2>
+
+    <label for = "login"></label>
+    <input type = "text" name = "login" required>
+    <br>
+    <label for = "password"></label>
+    <input type = "text" name = "password" required>
     
+    <button type="submit">Accedi</button>
+
+  
+
 </body>
 </html>
--- GestioneCarrello/oggetti.php
+++ GestioneCarrello/oggetti.php
@@ -1,2 +1,22 @@
 <?php
+
+$path = "oggetti.json";
+if(!file_exists($path))
+    die("errore il file non esiste");
+else{
+    //leggere contenuto
+    $json = file_get_contents("ogetti.json");
+    //var_dump($contenuto);
+    $dati = json_decode($json, true);
+    //var_dump($dati);
+    foreach($dati as $valore){
+        echo("<p>");
+        foreach($valore as $k => $v){
+            echo("$k: $v</br>");
+        }
+     echo("</p>");
+
+    }
+}
+
 ?>
