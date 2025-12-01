a8eefb636ac47a7cdcb526c6840fc65961210f50
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-12-01 12:43:21 +0100

    w

--- GestioneCarrello/mostraCarrello.php
+++ GestioneCarrello/mostraCarrello.php
@@ -1,2 +1,40 @@
 <?php
+    session_start();
+
+    $prodotti = json_decode(file_get_contents("oggetti.json"), true);
+
+
+    if (!isset($_SESSION["carrello"]) || empty($_SESSION["carrello"])) {
+        echo "<h2>Il carrello è vuoto</h2>";
+        echo '<a href="prodotti.php">Torna ai prodotti</a>';
+        exit;
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrello</title>
+</head>
+<body>
+    <h2>Carrello:</h2>
+
+    <?php foreach ($_SESSION["carrello"] as $id => $quantita): ?>
+    <?php 
+        foreach ($prodotti as $p) {
+            if ($p["id"] == $id) {
+                echo "<p><strong>{$p["nome"]}</strong> ({$p["categoria"]}) — Quantità: $quantita</p>";
+            }
+        }
+    ?>
+    <?php endforeach; ?>
+
+<form action="oggetti.php">
+    <button type="submit">Torna ai prodotti</button>
+</form>
+
+</body>
+</html>
+
+
--- GestioneCarrello/oggetti.php
+++ GestioneCarrello/oggetti.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    $prodotti = json_decode(file_get_contents("prodotti.json"), true);
+    $prodotti = json_decode(file_get_contents("oggetti.json"), true);
 
     if (!isset($_SESSION["carrello"])) {
         $_SESSION["carrello"] = [];
@@ -27,9 +27,22 @@
 </head>
 <body>
     <h2>PRODOTTI:</h2>
-    
 
-    <form action = "mostraCarrello" method = "POST">
+  <?php foreach ($prodotti as $p): ?>
+    <div style="margin-bottom: 10px;">
+        <b>ID:</b><?= $p["id"] ?> 
+        <b>Nome:</b><?= $p["nome"] ?>
+        <b>Categoria:</b><?= $p["categoria"] ?>
+       
+        <form action="" method="POST" style="display:inline;">
+            <input type="hidden" name="id" value="<?= $p["id"] ?>">
+            <button type="submit">Aggiungi al carrello</button>
+        </form>
+    </div>
+    <?php endforeach; ?>
+
+    <br>
+    <form action = "mostraCarrello.php" method = "POST">
     <button type="submit">Mostra Carrello</button>
     </form>
 
