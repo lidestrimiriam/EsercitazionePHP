48f496dd80b5e08d37f18cb717c15805c8a05ccb
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-12-01 13:11:14 +0100

    Update mostraCarrello.php

--- GestioneCarrello/mostraCarrello.php
+++ GestioneCarrello/mostraCarrello.php
@@ -9,6 +9,19 @@
         echo '<a href="oggetti.php">Torna ai prodotti</a>';
         exit;
     }
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["quantita"])) {
+    foreach ($_POST["quantita"] as $id => $q) {
+        $q = intval($q);
+            if ($q > 0) {
+                $_SESSION["carrello"][$id] = $q;
+            } else {
+                unset($_SESSION["carrello"][$id]);
+            }   
+        }
+        header("Location: mostraCarrello.php");
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,17 +33,23 @@
 <body>
     <h2>Carrello:</h2>
 
+    <form action="mostraCarrello.php" method="POST">
     <?php foreach ($_SESSION["carrello"] as $id => $quantita): ?>
-    <?php 
-        foreach ($prodotti as $p) {
-            if ($p["id"] == $id) {
-               echo "<p><strong>" . $p["nome"] . "</strong> (" . $p["categoria"] . ") — Quantità: " . $quantita . "</p>";
+        <?php 
+            foreach ($prodotti as $p) {
+                if ($p["id"] == $id) {
+                    echo "<p><strong>" . $p["nome"] . "</strong> (" . $p["categoria"] . ") — Quantità: " . $quantita . "</p>";
+                    echo ' — Quantità: <input type="number" name="quantita['.$id.']" value="'.$quantita.'" min="0">';
+                    echo '</p>';
+                }
             }
-        }
-    ?>
+        ?>
     <?php endforeach; ?>
+    <button type="submit">Aggiorna / Ricarica Carrello</button>
+    </form>
 
-<form action="oggetti.php">
+    <br>
+    <form action="oggetti.php">
     <button type="submit">Torna ai prodotti</button>
 </form>
 
