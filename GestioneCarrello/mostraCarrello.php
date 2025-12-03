ddd8335c1ef3fd8be26395445c20cb62546d28bc
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2025-12-03 13:37:00 +0100

    pp

--- GestioneCarrello/mostraCarrello.php
+++ GestioneCarrello/mostraCarrello.php
@@ -92,4 +92,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["quantita"])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
+ree
