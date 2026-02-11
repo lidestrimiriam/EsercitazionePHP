4fa4fb45d69bafedcc8cd98da391890c9cc39658
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-02-11 13:33:56 +0100

    ,k

--- 5BIWebApp/Biblioteca/inserisci.php
+++ 5BIWebApp/Biblioteca/inserisci.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "","Biblioteca");
+$conn = mysqli_connect("localhost", "root", "", "Biblioteca");
 
 $cf = $_POST['cf'];
 $nome = $_POST['nome'];
@@ -9,7 +9,15 @@ $email = $_POST['email'];
 $eta = $_POST['eta'];
 
 $query = "INSERT INTO Utente (cf, nome, cognome, email, eta) 
-VALUES ($cf, '$nome', '$cognome', '$email', $eta)"
+VALUES ($cf, '$nome', '$cognome', '$email', $eta)";
 
-$result = mysqli_query($con, $query);
+$result = mysqli_query($conn, $query);
+
+if($result){
+    echo "Inserimento riuscito";
+}else{
+    echo "Errore: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
