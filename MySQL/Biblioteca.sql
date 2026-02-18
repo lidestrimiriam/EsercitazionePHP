91af73f55591f9ae080d84455ed82250fe7a7de7
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-02-18 13:38:44 +0100

    Update Biblioteca.sql

--- MySQL/Biblioteca.sql
+++ MySQL/Biblioteca.sql
@@ -23,7 +23,7 @@ INSERT INTO Biblioteca.Utente values
 (12, 'Edoardo', 'Serra', 'e.serra@gmail.com', 11),
 (13, 'Noemi', 'Villa', 'n.villa@gmail.com', 10),
 (14, 'Samuele', 'Ferraro', 's.ferraro@gmail.com', 9),
-(15, 'Aurora', 'Fabbri', 'a.fabbri@gmail.com', 8);
+(15, 'Niccolo', 'Rossi', 'nicolo.rossi@gigi.com', 8);
 
 CREATE TABLE Biblioteca.Libro(
 	id int PRIMARY KEY,
@@ -41,14 +41,14 @@ INSERT INTO Biblioteca.Libro values
 (5, 'Il piccolo principe', 'Antoine de Saint-Exupéry', '9780156013987', 1943),
 (6, 'I promessi sposi', 'Alessandro Manzoni', '9788807901862', 1827),
 (7, 'Il calice di fuoco', ' J.K. Rowling', '9788845292613', 2005),
-(8, '1984', 'George Orwell', '9780451524936', 1949),
-(9, 'Orgoglio e pregiudizio', 'Jane Austen', '9780141439518', 1813),
-(10, 'Il grande Gatsby', 'F. Scott Fitzgerald', '9780743273565', 1925),
-(11, 'Moby Dick', 'Herman Melville', '9781503280786', 1851),
-(12, 'Dieci piccoli indiani', 'Agatha Christie', '9780062073488', 1939),
-(13, 'Il signore delle mosche', 'William Golding', '9780571056866', 1954),
-(14, 'Il codice Da Vinci', 'Dan Brown', '9780307474278', 2003),
-(15, 'La coscienza di Zeno', 'Italo Svevo', '9788807881553', 1923);
+(8, 'Dr. Jekyll and Mr. Hyde', 'Robert Louis Stevenson', '9780451524936', 1886),
+(9, 'L.isola del tesoro', 'Robert Louis Stevenson', '9780618640157', 1883),
+(10, 'Il castello errante di Howl', 'Diana Wynne Jones', '9780743273565', 1986),
+(11, 'Assassinio sull’Orient Express', 'Agatha Christie', '9781503280786', 1934),
+(12, 'Bianco come il latte rosso come il sangue', ' Alessandro D.Avenia', '9780062073488', 2010),
+(13, 'Ciò che inferno non è', 'Alessandro D.Avenia', '9780571056866', 2014),
+(14, 'Fino a quando la mia stella brillerà', 'Daniela Palumbo', '9780307474278', 2015),
+(15, 'Viaggio al centro della Terra', 'Jules Verne', '9788807881553', 1864);
 
 CREATE TABLE Biblioteca.Prestito(
 	id_prestito int PRIMARY KEY,
@@ -68,24 +68,28 @@ CREATE TABLE Biblioteca.Prestito(
 );
 
 INSERT INTO Biblioteca.Prestito values 
-(1, 1, 1, '2025-05-10', '2026-01-18'),
-(2, 2, 3, '2025-01-15', '2025-02-15'),
-(3, 3, 1, '2025-03-01', '2025-03-20'),
+(1, 1, 1, '2024-05-10', '2025-01-18'),
+(2, 2, 3, '2026-01-15', '2026-02-15'),
+(3, 3, 1, '2024-03-01', '2024-03-20'),
 (4, 4, 2, '2025-04-10', '2025-05-10'),
 (5, 1, 4, '2025-06-01', NULL),
-(6, 5, 2, '2025-02-10', '2025-03-01'),
+(6, 5, 2, '2024-02-10', '2024-03-01'),
 (7, 6, 5, '2025-03-15', '2025-04-10'),
-(8, 7, 6, '2025-04-01', '2025-04-20'),
-(9, 8, 7, '2025-05-05', '2025-05-25'),
+(8, 7, 6, '2026-04-01', '2026-04-20'),
+(9, 8, 7, '2024-05-05', '2024-05-25'),
 (10, 9, 8, '2025-06-10', NULL),
-(11, 10, 9, '2025-01-20', '2025-02-05'),
-(12, 11, 10, '2025-02-14', '2025-03-14'),
+(11, 10, 9, '2026-01-20', '2026-02-05'),
+(12, 11, 10, '2024-02-14', '2024-03-14'),
 (13, 12, 3, '2025-03-22', '2025-04-12'),
-(14, 13, 4, '2025-04-18', '2025-05-02'),
-(15, 14, 11, '2025-05-25', NULL);
+(14, 13, 4, '2026-04-18', '2026-05-02'),
+(15, 14, 11, '2024-05-25', NULL);
 
 SELECT * FROM Biblioteca.Utente;
 SELECT * FROM Biblioteca.Libro;
 SELECT * FROM Biblioteca.Utente WHERE eta <= 18;
-SELECT * FROM Biblioteca.Libro WHERE anno_pubblicazione >= 2010;
-SELECT * FROM Biblioteca.Utente WHERE email LIKE '%@gmail.com';
+SELECT * FROM Biblioteca.Libro WHERE anno_pubblicazione > 2010;
+SELECT nome, cognome, eta FROM Biblioteca.Utente WHERE email LIKE '%@gmail%';
+
+SELECT * FROM Biblioteca.Utente WHERE cognome LIKE 'B%';
+SELECT DISTINCT Libro.titolo FROM Biblioteca.Libro JOIN Biblioteca.Prestito ON libro.id_utente = prestito.id_libro WHERE prestito.data_prestito LIKE '2026%';
+SELECT * FROM Biblioteca.Prestito JOIN Biblioteca.Utente ON Prestito.id = Utente.cf WHERE Utente.email = 'nicolo.rossi@gigi.com';
