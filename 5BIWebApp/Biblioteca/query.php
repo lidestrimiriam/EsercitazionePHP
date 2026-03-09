74eab5f150864c8b062018a532b9b23c29ae5cd4
lidestrimiriam <lidestri.miriam@studenti.salesianisesto.it>
2026-03-09 12:35:15 +0100

    Update query.php

--- 5BIWebApp/Biblioteca/query.php
+++ 5BIWebApp/Biblioteca/query.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ETA PERSONE</title>
 </head>
 <body>
-    nome: <input type = "text" name = "nome">
-    cognome: <input type = "text" name = "cognome">
-        
-    eta: <input type = "number" name = "eta">
-    <input type = "submit" name = "invia" value = "invia dati">
-</body>
-</html>
 
-
+<form method="get">
+    Nome: <input type="text" name="nome"><br>
+    Cognome: <input type="text" name="cognome"><br>
+    Eta: <input type="number" name="eta"><br>
+    <input type="submit" name="invia" value="Invia dati">
+</form>
 
 <?php
 $conn = mysqli_connect("localhost", "root", "", "Biblioteca");
 
-if(isset($GET['eta'])){
+if(isset($_GET['eta'])){
     $eta = $_GET['eta'];
-    $query = ("SELECT nome, cognome, eta FROM Utente WHERE eta = $eta");
+
+    $query = "SELECT nome, cognome, eta FROM Utente WHERE eta = $eta";
     $result = mysqli_query($conn, $query);
 
-    if($result -> num_rows > 0){
-        while($riga = $result -> fetch_assoc()){
+    if(mysqli_num_rows($result) > 0){
+        while($riga = mysqli_fetch_assoc($result)){
             echo "Nome: ".$riga['nome']."<br>";
             echo "Cognome: ".$riga['cognome']."<br>";
-            echo "Eta: ".$riga['eta']."<br>";
+            echo "Eta: ".$riga['eta']."<br><br>";
         }
-    }else{
+    } else {
         echo "Nessun utente trovato per l'età $eta";
     }
 }
 ?>
+
+</body>
+</html>
